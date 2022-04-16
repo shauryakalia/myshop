69980903ec7ccae22471f0e77efbca7cf2bed7ca
shauryakalia <shauryakalia@gmail.com>
2022-04-16 16:46:05 +0530

    main menu

---
+++ src/components/menu/menu.component.jsx
@@ -0,0 +1,16 @@
+import './menu.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+const Menu = ({ categories }) => {
+    return (
+        <div className='menu-container'>
+            {
+                categories.map((category) => (
+                    <CategoryItem key={category.id} category={category} />
+                ))
+            }
+        </div>
+    )
+}
+
+export default Menu;
