a5341672f7703f8f2b7ce8d54574a3250db257e7
shauryakalia <shauryakalia@gmail.com>
2022-04-17 02:33:27 +0530

    base structure

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import Menu from './components/menu/menu.component';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/routes/home/home.component';
+import Navbar from './components/routes/navbar/navbar.component';
 
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
-  
   return (
-    <div className="App">
-      <Menu categories={categories} />
-    </div>
+    <Routes>
+      <Route path='/' element={ <Navbar /> }>
+        <Route index element={<Home />}/>
+      </Route>
+    </Routes>
   );
 }
 
